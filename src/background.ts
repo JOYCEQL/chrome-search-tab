cd59450d17cfe29659c317e4e88d3794d407b413
yuguangzhou <yuguangzhou@yiwise.com>
2024-06-02 23:57:45 +0800

    feat: ui && icon

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,13 @@
 console.log('background service worker 222')
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === 'toggle') {
+      // 执行对应的操作
+      console.log(command,'command')
+      // 将popup.html作为shadow dom插入
+      chrome.tabs.create({
+        url: chrome.runtime.getURL('index.html')
+      });
+
+    }
+  });
