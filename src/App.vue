4daad825a360916e6adba61ecbf4417c7a82808a
yuguangzhou <yuguangzhou@yiwise.com>
2024-06-03 23:07:32 +0800

    feat: add filter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { CircleX } from 'lucide-vue-next';
+import { computed, onMounted, ref } from 'vue'
+import { CircleX } from 'lucide-vue-next'
 const openedTabs = ref<any>([])
+const searchValue = ref<string>('')
 
 onMounted(() => {
+  getAactivedTab()
+})
+
+const getAactivedTab = () => {
   chrome.tabs.query({ windowType: 'normal' }, function (tabs: any) {
-    console.log(tabs, 'tabs')
     openedTabs.value = []
     openedTabs.value = tabs
   })
-})
+}
 
 const goTab = (tab: any) => {
   chrome.tabs.update(tab.id, { active: true })
   chrome.windows.update(tab.windowId, { focused: true })
 }
+
+const resultTab = computed(() => {
+  console.log(
+    openedTabs.value.filter((tab) => tab.title.toLowerCase().includes(searchValue.value.toLowerCase())),
+    11
+  )
+  return openedTabs.value.filter((tab) => tab.title.toLowerCase().includes(searchValue.value.toLowerCase()))
+})
 </script>
 
 <template>
   <div class="w-full">
-    <NInput autofocus class="mb-4 rounded-[8px]" placeholder="输入URL或者标题搜索">naive-ui</NInput>
-    <div v-for="tab in openedTabs" :key="tab.id"
-      class="list-item bg-[#000212] p-4  mb-[12px] rounded-[10px] cursor-pointer"
-      @click="goTab(tab)">
+    <NInput autofocus v-model:value="searchValue" class="mb-4 rounded-[8px]" placeholder="输入URL或者标题搜索"
+      >naive-ui</NInput
+    >
+    <div
+      v-for="tab in resultTab"
+      :key="tab.id"
+      class="list-item bg-[#000212] p-4 mb-[12px] rounded-[10px] cursor-pointer"
+      @click="goTab(tab)"
+    >
       {{ tab.title }}
       <CircleX></CircleX>
     </div>
@@ -34,7 +51,7 @@ const goTab = (tab: any) => {
   display: flex;
   justify-content: space-between;
   border: 1px rgb(57, 55, 54) solid;
-  &:hover{
+  &:hover {
     color: green;
     font-weight: 700;
     border-color: green;
